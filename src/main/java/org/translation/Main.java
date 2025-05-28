5a3f7b24bf44070f8292f7b8fa575d83675582de
Ethan Hapurne <ethan.hapurne@gmail.com>
2025-05-28 18:52:50 -0400

    Fixed main

--- src/main/java/org/translation/Main.java
+++ src/main/java/org/translation/Main.java
@@ -2,8 +2,6 @@ package org.translation;
 
 import java.util.List;
 import java.util.Scanner;
-import java.util.Collections;
-
 
 /**
  * Main class for this program.
@@ -16,20 +14,13 @@ import java.util.Collections;
  */
 public class Main {
 
-    private static final String QUIT = "quit";
-
     /**
      * This is the main entry point of our Translation System!<br/>
      * A class implementing the Translator interface is created and passed into a call to runProgram.
-     *
      * @param args not used by the program
      */
     public static void main(String[] args) {
-
-
-        Translator translator = new JSONTranslator(null);
-
-
+        Translator translator = new JSONTranslator();
         runProgram(translator);
     }
 
@@ -37,39 +28,40 @@ public class Main {
      * This is the method which we will use to test your overall program, since
      * it allows us to pass in whatever translator object that we want!
      * See the class Javadoc for a summary of what the program will do.
-     *
      * @param translator the Translator implementation to use in the program
      */
     public static void runProgram(Translator translator) {
         while (true) {
+            String quit = "quit";
             String country = promptForCountry(translator);
-
-            if (QUIT.equals(country)) {
+            if (quit.equals(country)) {
                 break;
             }
-
-            String language = promptForLanguage(translator, country);
-            if (QUIT.equals(language)) {
+            String countryCode = new CountryCodeConverter().fromCountry(country);
+            String language = promptForLanguage(translator, countryCode);
+            if (quit.equals(language)) {
                 break;
             }
-
-            System.out.println(country + " in " + language + " is " + translator.translate(country, language));
+            String languageCode = new LanguageCodeConverter().fromLanguage(language);
+            System.out.println(country + " in " + language + " is " + translator.translate(countryCode, languageCode));
             System.out.println("Press enter to continue or quit to exit.");
             Scanner s = new Scanner(System.in);
             String textTyped = s.nextLine();
 
-            if (QUIT.equals(textTyped)) {
+            if (quit.equals(textTyped)) {
                 break;
             }
         }
     }
 
-
-
+    // Note: CheckStyle is configured so that we don't need javadoc for private methods
     private static String promptForCountry(Translator translator) {
         List<String> countries = translator.getCountries();
-        Collections.sort(countries);
 
+        CountryCodeConverter converter = new CountryCodeConverter();
+        countries.replaceAll(converter::fromCountryCode);
+
+        countries.sort(String::compareTo);
         for (String country : countries) {
             System.out.println(country);
         }
@@ -78,12 +70,17 @@ public class Main {
 
         Scanner s = new Scanner(System.in);
         return s.nextLine();
+
     }
 
+    // Note: CheckStyle is configured so that we don't need javadoc for private methods
     private static String promptForLanguage(Translator translator, String country) {
         List<String> languages = translator.getCountryLanguages(country);
-        Collections.sort(languages);
 
+        LanguageCodeConverter converter = new LanguageCodeConverter();
+        languages.replaceAll(converter::fromLanguageCode);
+
+        languages.sort(String::compareTo);
         for (String language : languages) {
             System.out.println(language);
         }
